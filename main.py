789f814a780a57600ede6d4d6be1d1df04cb4dce
Alex Plate <AlexPl292@gmail.com>
2021-05-08 19:16:30 +0300

    Support tap command and return code

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ def main():
     command = sys.argv[1:]
 
     command.insert(0, "brew")
-    subprocess.run(command)
+    result = subprocess.run(command)
+    return_code = result.returncode
+
+    if return_code != 0:
+        return
 
     if len(command) <= 1:
         return
 
-    if command[1] == "install":
+    if command[1] == "install" and len(command) > 2:
         package = command[2]
-        add_to_brew("brew \"" + package + "\"")
+        if len(package) > 0:
+            add_to_brew("brew \"" + package + "\"")
+    elif command[1] == "tap" and len(command) > 2:
+        package = command[2]
+        if len(package) > 0:
+            add_to_brew("tap \"" + package + "\"")
 
 
 def add_to_brew(data):
     with open(str(Path.home()) + "/Brewfile", "a") as brewfile:
-        brewfile.write(data)
+        brewfile.write(data + "\n")
 
 
 if __name__ == '__main__':
