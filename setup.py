f2058f51496134ceab9022925703d6f9ddfb431b
Alex Plate <AlexPl292@gmail.com>
2021-05-08 20:38:45 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="rew",
+    version="0.1.0",
+    description="Keep your Brewfile up-to-date",
+    author="Alex Plate",
+    author_email="AlexPl292@gmail.com",
+    url="https://github.com/AlexPl292/rew",
+)
